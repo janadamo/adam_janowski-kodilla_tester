2317f5c9156a9f561fe95abf8bfbee91b66e2a55
Adam Janowski <adam.janowski@hotmail.co.uk>
2023-03-09 19:23:24 +0100

    prework

--- kodilla-intro/src/main/java/LeapYear.java
+++ kodilla-intro/src/main/java/LeapYear.java
@@ -4,11 +4,17 @@ public class LeapYear {
 
         System.out.println("Check if given year is a leap year.");
         System.out.println("Year : " + year);
-
+        boolean isLeapYear = isLeapYear(year);
+        System.out.println(isLeapYear);
+    }
+    public static boolean isLeapYear (int year) {
         if(year%4 ==0 && year%100 !=0 || year%400 ==0) {
             System.out.println(year + " is a leap year.");
+            return true;
         } else {
             System.out.println(year+ " is not a leap year.");
+            return false;
         }
+
     }
 }
