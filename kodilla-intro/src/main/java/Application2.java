569da692483ca71f006033dec16718965090b421
Adam Janowski <adam.janowski@hotmail.co.uk>
2023-03-23 22:52:37 +0100

    homework

---
+++ kodilla-intro/src/main/java/Application2.java
@@ -0,0 +1,6 @@
+public class Application2 {
+    public static void main(String[] args) {
+        Application application = new Application("Adam", 29, 170);
+        application.checkUser();
+    }
+}
